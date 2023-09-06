bae96106945c6de2dbffd3aa4d5fd87dd438ba6b
GordieFilin <filinegor@gmail.com>
2023-09-06 19:26:58 +0300

    changes from main merge

---
+++ src/main/java/oop/CatParent.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class CatParent implements Animals {
+    @Override
+    public int legsQuantity() {
+        return 4;
+    }
+
+    @Override
+    public String voice() {
+        return "myayyyy";
+    }
+
+    public int jumpHigh(){
+
+        return 3;
+    }
+}
---
+++ src/main/java/oop2/SnakeExtendsAbstract.java
@@ -0,0 +1,9 @@
+package oop2;
+
+public class SnakeExtendsAbstract extends Abstract{
+    @Override
+    public int legsQuantity() {
+        return 0;
+    }
+
+}
